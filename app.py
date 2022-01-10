83335a6ebaab827d953403037bdb14b734df4190
Nancy Williams <59902324+ognancy4life@users.noreply.github.com>
2022-01-09 21:54:34 -0800

    add plotting and beautify

--- app.py
+++ app.py
@@ -5,55 +5,47 @@ import pandas as pd
 import requests
 import json
 from glom import glom
+from datetime import datetime
 from bokeh.plotting import figure
 
+st.set_page_config(page_icon="📈", layout="wide")
 st.title("Day 10 Milestone Project")
-st.header("by Nancy Williams")
+st.subheader("by Nancy Williams")
 st.write('This app demonstrates pulling stock data from Alpha Advantage via an API and displaying using Streamlit, Pandas, and Bokeh on a Heroku app.')
-# st.header("Customary quote")
-# st.markdown("> I just love to go home, no matter where I am [...]")
+st.caption("*Note that these values are unadjusted, meaning that they do not correct for splits. Looking at the stock's trends over long periods of time can be misleading. The adjusted data are a premium feature.")
 
 # Need to set default ticker to something like AAPL
-st.text_input("Enter the stock's abbreviation (all capital letters):", key='ticker')
-# You can access the value at any point with: st.session_state.ticker
+st.sidebar.header('Set plot parameters:')
+ticker='MSFT'
+if st.sidebar.text_input("Enter the stock's abbreviation (e.g. MSFT):", key='ticker'):
+     ticker = st.session_state.ticker
 
-st.write("Select the time period you wish to plot:")
-neighborhood = st.radio("Time Period", ['1 day', '1 week', '1 month', '1 year'])
-
-#ALPHAVANTAGE_API_KEY = os.environ['ALPHAVANTAGE_API_KEY']
-#st.write(ALPHAVANTAGE_API_KEY)
+# Help at https://devcenter.heroku.com/articles/config-vars
+# API key must be named ALPHAVANTAGE_API_KEY
 ts = TimeSeries(output_format='pandas')
+data, meta_data = ts.get_daily(ticker, outputsize='full')
+# you should use get_daily_adjusted but this is premium and I don't want to pay :)
 
-ticker = st.session_state.ticker
-data, meta_data = ts.get_intraday(ticker)
+st.sidebar.write("Select the time period you wish to plot:")
+period = st.sidebar.slider('Date Range',
+     value=[datetime(1999,9,1), datetime(2022,4,1)])
 
-#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}&datatype=csv'.format(ticker, key)
-#response = requests.get(url)
-# data = response.json()
-#with open(str(data),'r') as f:
-#    data2 = json.loads(f.read())
-    
-#df = pd.json_normalize(
-#    data['Time Series (Daily)'],
-#    meta='Meta Data'
-#    ).T
-#df = pd.read_json(data, orient='records')
-st.write(data)
-#st.write(df['Time Series (Daily)'].apply(lambda row: glom(row, '4. close')))
-#print(response.json())
-#st.write(response.json())
-
-# Need to figure out how to get only fields including "close" and parse the dates
-
-# example for later https://github.com/jiayi-ren/stock-scraper/blob/master/scraper.py
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
+# Plot the data
 p = figure(
-     title='simple line example',
-     x_axis_label='x',
-     y_axis_label='y')
+     #title='simple line example',
+     x_axis_label='Time',
+     x_axis_type='datetime',
+     y_axis_label='Price ($)',
+     x_range=period,
+     title=ticker)
 
-p.line(x, y, legend_label='Trend', line_width=2)
-
+p.title.text_font_size = '20pt'
+p.line(data.index, data['4. close'], 
+     legend_label='Closing', 
+     line_width=1.5)
 st.bokeh_chart(p, use_container_width=True)
+# example https://docs.streamlit.io/library/api-reference/charts/st.bokeh_chart
+
+# show dataframe for funsies
+if st.checkbox('Show raw data'):
+    data
